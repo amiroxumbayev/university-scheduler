c1233501110d20f562904d1689097007bdd01b00
amiroxumbayev <amiroxumbayev@gmail.com>
2022-10-20 23:44:22 +0600

    Fulfilled implementation crud for subject

--- src/main/java/com/example/universityscheduler/service/SubjectService.java
+++ src/main/java/com/example/universityscheduler/service/SubjectService.java
@@ -1,6 +1,8 @@
 package com.example.universityscheduler.service;
 
 import com.example.universityscheduler.domain.dto.SubjectDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
@@ -9,4 +11,6 @@ public interface SubjectService {
     SubjectDTO save(SubjectDTO subjectDTO);
 
     SubjectDTO findById(UUID id);
+
+    Page<SubjectDTO> findAll(Pageable pageable);
 }
